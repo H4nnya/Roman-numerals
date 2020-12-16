e37567469765a4681d25ef0ddd5c4b408211cce4
Say, The Creator <64602785+H4nnya@users.noreply.github.com>
2020-12-16 01:18:41 -0300

    Add files via upload

---
+++ roman numerals.py
@@ -0,0 +1,45 @@
+
+
+def roman(values):
+    value = str(values)
+    symbol = {1: 'I', 2: 'II', 3: 'III', 4: 'IV', 5: 'V', 6: 'VI', 7: 'VII', 8: 'VIII', 9: 'IX',
+              10: 'X', 20: 'XX', 30: 'XXX', 40: 'XL', 50: 'L', 60: 'LX', 70: 'LXX', 80: 'LXXX', 90: 'XC',
+              100: 'C', 200: 'CC', 300: 'CCC', 400: 'CD', 500: 'D', 600: 'DC', 700: 'DCC', 800: 'DCCC', 900: 'CM',
+              1000: 'M', 2000: 'MM', 3000: 'MMM'}
+
+    size = len(value)
+    result = ''
+    if size == 1:
+        result = str(symbol[int(value)])
+
+    elif size == 2:
+        result = str(symbol[int(value[0]) * 10])
+        if int(value[1]) >= 1:
+            result += str(symbol[int(value[1])])
+
+    elif size == 3:
+        result = str(symbol[int(value[0]) * 100])
+        if int(value[1]) >= 1:
+            result += str(symbol[int(value[1]) * 10])
+        if int(value[2]) >= 1:
+            result += str(symbol[int(value[2])])
+
+    elif size == 4:
+        result = str(symbol[int(value[0]) * 1000])
+        if int(value[1]) >= 1:
+            result += str(symbol[int(value[1]) * 100])
+        if int(value[2]) >= 1:
+            result += str(symbol[int(value[2]) * 10])
+        if int(value[3]) >= 1:
+            result += str(symbol[int(value[3])])
+    return result
+
+
+while True:
+    print('\033[36mDigite um número entre 1 a 3999.\033[m[ou 0]')
+    num = input('>> ')
+
+    if int(num) == 0:
+        break
+
+    print(f'O valor {num} representa \033[32m{roman(num)}\033[m em algarismos romano.')
