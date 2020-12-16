018bd65e7e1082fcae8104bc7821a2eebec9bde2
Say, The Creator <64602785+H4nnya@users.noreply.github.com>
2020-12-16 01:24:11 -0300

    Update roman numerals.py

--- roman numerals.py
+++ roman numerals.py
@@ -1,7 +1,7 @@
 
 
-def roman(values):
-    value = str(values)
+def roman(number=int()):
+    value = str(number)
     symbol = {1: 'I', 2: 'II', 3: 'III', 4: 'IV', 5: 'V', 6: 'VI', 7: 'VII', 8: 'VIII', 9: 'IX',
               10: 'X', 20: 'XX', 30: 'XXX', 40: 'XL', 50: 'L', 60: 'LX', 70: 'LXX', 80: 'LXXX', 90: 'XC',
               100: 'C', 200: 'CC', 300: 'CCC', 400: 'CD', 500: 'D', 600: 'DC', 700: 'DCC', 800: 'DCCC', 900: 'CM',
@@ -37,7 +37,7 @@ def roman(values):
 
 while True:
     print('\033[36mDigite um número entre 1 a 3999.\033[m[ou 0]')
-    num = input('>> ')
+    num = int(input('>> '))
 
     if int(num) == 0:
         break
